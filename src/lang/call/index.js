bb22fb376890290a739e83a11453eb20f6582664
Jan Klaas Kollhof <jan@kollhof.net>
2020-02-11 17:07:48 +0100

    fix(pipe): use lefthand infix operator for pipe

--- src/lang/call/index.js
+++ src/lang/call/index.js
@@ -2,11 +2,11 @@ import {
   add_operator_like, symbol, add_non_binding
 } from '@fink/prattler/symbols';
 
-import {infix_right} from '../generic/infix';
+import {infix} from '../generic/infix';
 import {call} from './call';
 
 
-const pipe = (op)=> infix_right(op, 'pipe');
+const pipe = (op)=> infix(op, 'pipe');
 
 
 export const add_call_operators = (ctx)=> (
