976d69752ccb6b5fcafcd59a7d34afd82c234ed1
Jan Klaas Kollhof <jan@kollhof.net>
2020-02-23 17:58:02 +0100

    refactor(group): rename group module as it no longer handles func

--- src/lang/group/index.js
+++ src/lang/group/index.js
@@ -1,8 +1,22 @@
-import {
-  add_operator_like, symbol, add_non_binding
-} from '@fink/prattler/symbols';
+import {add_operator_like, add_non_binding} from '@fink/prattler/symbols';
+import {next_is, curr_loc} from '@fink/prattler';
 
-import {group} from './group-or-func';
+import {symbol} from '../symbols';
+import {seq} from '../generic/sequence';
+import {get_block} from '../generic/block';
+
+
+export const group = (op)=> ({
+  ...symbol(op),
+
+  nud: ()=> (ctx)=> {
+    const {start} = curr_loc(ctx);
+    const [exprs, next_ctx] = seq(ctx, ')');
+    const {end} = curr_loc(next_ctx);
+
+    return [{type: 'group', exprs, loc: {start, end}}, next_ctx];
+  }
+});
 
 
 export const add_group = (ctx)=> (
