a3f1371296b055d0b560cde130d81ed560046849
Jan Klaas Kollhof <jan@kollhof.net>
2020-02-28 13:44:18 +0100

    feat(numbers,identifiers): add support for floats, hex, oct, bin numbers
    BREAKING CHANGE: number nodes are of type `number` and all other tokens are treated as `ident`
    rather then `Symbol('other')`

--- src/lang/identifier/other.js
+++ src/lang/identifier/other.js
@@ -1,6 +1,8 @@
 import {
-  curr_value, curr_loc, expression, next_matches, next_loc
+  curr_value, curr_loc, expression, next_matches, next_loc, next_is,
+  assert_advance, advance
 } from '@fink/prattler';
+import {token_error} from '@fink/prattler/errors';
 import {other_token, next_lbp} from '@fink/prattler/symbols';
 
 import {symbol} from '../symbols';
@@ -20,15 +22,57 @@ const infix_led = (lbp)=> (ctx, left)=> {
 };
 
 
+const looks_like_num = (ctx)=> {
+  const value = curr_value(ctx);
+  return value.match(/^[0-9]+.*/) !== null;
+};
+
+
+// eslint-disable-next-line max-statements
+const number = (ctx)=> {
+  let value = curr_value(ctx);
+  const {start} = curr_loc(ctx);
+
+  if (next_is(ctx, '.')) {
+    ctx = advance(ctx);
+    value += curr_value(ctx);
+    ctx = advance(ctx);
+    value += curr_value(ctx);
+
+    if (value.endsWith('e')) {
+      ctx = advance(ctx);
+      value += curr_value(ctx);
+
+      if (curr_value(ctx) === '+' || curr_value(ctx) === '-') {
+        ctx = advance(ctx);
+        value += curr_value(ctx);
+      } else {
+        throw token_error(
+          `Expected exponent:`,
+          ctx.curr_token, ctx
+        );
+      }
+    }
+  }
+
+  const {end} = curr_loc(ctx);
+  return [{type: 'number', value, loc: {start, end}}, ctx];
+};
+
+
 export const other = ()=> ({
   ...symbol(other_token),
 
+  // eslint-disable-next-line max-statements
   nud: ()=> (ctx)=> {
     const value = curr_value(ctx);
     const loc = curr_loc(ctx);
 
-    // TODO: use 'other' instead of other_token
-    return [{type: other_token, value, loc}, ctx];
+    if (looks_like_num(ctx)) {
+      return number(ctx);
+    }
+
+    return [{type: 'ident', value, loc}, ctx];
   },
 
   led: (lbp)=> infix_led(lbp-1)
