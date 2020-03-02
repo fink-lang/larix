e7c0869e7b2defe5bd0365362c0197a30b066823
Jan Klaas Kollhof <jan@kollhof.net>
2020-03-02 12:22:12 +0100

    fix(js operators): make `new` and `throw` non-separating operators

--- src/lang/js-compat/index.js
+++ src/lang/js-compat/index.js
@@ -1,12 +1,10 @@
-import {
-  add_operator_like, add_non_binding, add_non_separating
-} from '@fink/prattler/symbols';
+import {add_non_separating} from '@fink/prattler/symbols';
 
 import {prefix} from '../generic/prefix';
 
 
 export const add_js_ops = (ctx)=> (
   ctx
-    |> add_operator_like(prefix('new', 'new'))
-    |> add_operator_like(prefix('throw', 'throw'))
+    |> add_non_separating(prefix('new', 'new'))
+    |> add_non_separating(prefix('throw', 'throw'))
 );
